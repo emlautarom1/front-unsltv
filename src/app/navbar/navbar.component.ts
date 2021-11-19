61f7900ad6e99ff0fb1028f30811ad669a7a589a
Lautaro Emanuel <emlautarom1@gmail.com>
2021-11-19 17:47:58 -0300

    Navbar dinamica
    - Color de fondo depende del estado del scroll

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IconDefinition, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { distinct, distinctUntilChanged, fromEvent, map, Observable, startWith, Subscriber } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   searchIcon: IconDefinition = faSearch;
+  didScroll!: Observable<boolean>;
 
   constructor() { }
+
+  ngOnInit(): void {
+    this.didScroll = fromEvent(window, "scroll").pipe(
+      startWith(false),
+      map(() => window.scrollY > 50),
+      distinctUntilChanged());
+  }
 
 }
