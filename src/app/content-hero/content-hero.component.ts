7fa95e5c788f11cd134b52cbfaeeb4141a6a1a0d
Lautaro Emanuel <emlautarom1@gmail.com>
2021-11-19 18:15:51 -0300

    Actualizado CSS componente 'Hero'
    - Se agregaron iconos
    - Se ajustaron algunos efectos en CSS

--- src/app/content-hero/content-hero.component.ts
+++ src/app/content-hero/content-hero.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { faPlayCircle, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-content-hero',
   templateUrl: './content-hero.component.html',
   styleUrls: ['./content-hero.component.scss']
 })
-export class ContentHeroComponent implements OnInit {
+export class ContentHeroComponent {
+  faPlayCircle = faPlayCircle;
+  faInfoCircle = faInfoCircle;
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
