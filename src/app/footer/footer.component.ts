d0b9e4b695569b5429d91c3741072a0d83756e9b
Lautaro Emanuel <emlautarom1@gmail.com>
2022-01-03 16:27:25 -0300

    Actualizado footer
    Añadida información de contacto e íconos de redes sociales

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { faFacebookF, faInstagram, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent {
+  socialNetworks = [
+    faFacebookF
+    , faInstagram
+    , faTwitter
+    , faYoutube
+  ]
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
