1dbfbd173763d7237ce2ce21163b37ccd0a2a07e
Lautaro Emanuel <emlautarom1@gmail.com>
2022-01-08 16:58:50 -0300

    Pagina "404" inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { SwiperModule } from 'swiper/angular';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
+import { AppComponent } from './app.component';
 import { ContentSliderComponent } from './page/home/content-slider/content-slider.component';
 import { ContentHeroComponent } from './page/home/content-hero/content-hero.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
-
-import { SwiperModule } from 'swiper/angular';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AboutComponent } from './page/about/about.component';
 import { HomeComponent } from './page/home/home.component';
 import { SearchResultsComponent } from './page/search-results/search-results.component';
 import { SingleResultComponent } from './page/search-results/single-result/single-result.component';
 import { WatchComponent } from './page/watch/watch.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -29,6 +29,7 @@ import { WatchComponent } from './page/watch/watch.component';
     SearchResultsComponent,
     SingleResultComponent,
     WatchComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
