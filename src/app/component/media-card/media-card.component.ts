8219361970924c7f43a73b481c2618b031166d26
Lautaro Emanuel <emlautarom1@gmail.com>
2022-01-08 18:10:11 -0300

    Usar "@Input()" para configurar "MediaCard"

--- src/app/component/media-card/media-card.component.ts
+++ src/app/component/media-card/media-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-media-card',
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./media-card.component.scss']
 })
 export class MediaCardComponent implements OnInit {
+  @Input() id = ""
+  @Input() thumbnail = ""
+  @Input() title = ""
+  @Input() date = ""
+  @Input() category = ""
+  @Input() description = ""
 
   constructor() { }
 
