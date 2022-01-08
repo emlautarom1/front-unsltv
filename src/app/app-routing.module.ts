c2af9110269aabfd61a017b4696e526561afb32d
Lautaro Emanuel <emlautarom1@gmail.com>
2022-01-08 16:59:12 -0300

    Navegación inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AboutComponent } from './page/about/about.component';
+import { HomeComponent } from './page/home/home.component';
+import { SearchResultsComponent } from './page/search-results/search-results.component';
+import { WatchComponent } from './page/watch/watch.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Home redirect
+  { path: "", redirectTo: "/home", pathMatch: "full" },
+
+  { path: "home", component: HomeComponent },
+  { path: "search/:query", component: SearchResultsComponent },
+  { path: "watch/:id", component: WatchComponent },
+  { path: "about", component: AboutComponent },
+
+  // 404 redirect
+  { path: "**", component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
