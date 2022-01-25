b69fb6850b70aafd9c0e41f6fb9b8c1d9ba6b553
Lautaro Emanuel <emlautarom1@gmail.com>
2022-01-25 18:03:06 -0300

    Usar datos desde servicio

--- src/app/page/search/search.component.ts
+++ src/app/page/search/search.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map, mergeMap, take, toArray } from 'rxjs/operators';
+import { Video } from 'src/app/model/video';
 import { YoutubeService } from 'src/app/service/youtube.service';
 
 @Component({
@@ -10,7 +11,7 @@ import { YoutubeService } from 'src/app/service/youtube.service';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
-  results$!: Observable<any[]>;
+  results$!: Observable<Video[]>;
 
   constructor(
     private route: ActivatedRoute,
