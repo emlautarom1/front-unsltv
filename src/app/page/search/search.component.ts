f8fa855c60621692616108011d6166e90ef8a9e1
Lautaro Emanuel <emlautarom1@gmail.com>
2022-01-26 20:23:37 -0300

    Ajustar el título de 'search' según el tipo de contenido

--- src/app/page/search/search.component.ts
+++ src/app/page/search/search.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
-import { map, switchMap, take, tap, toArray } from 'rxjs/operators';
+import { map, shareReplay, switchMap, take, tap, toArray } from 'rxjs/operators';
 import { SearchParams } from 'src/app/model/search';
 import { Video } from 'src/app/model/video';
 import { YoutubeService } from 'src/app/service/youtube.service';
@@ -12,6 +12,7 @@ import { YoutubeService } from 'src/app/service/youtube.service';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
+  title$!: Observable<string>;
   results$!: Observable<Video[]>;
 
   constructor(
@@ -20,9 +21,24 @@ export class SearchComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.results$ = this.route.queryParamMap.pipe(
+    const search$ = this.route.queryParamMap.pipe(
       map(params => { return { playlist: params.get("playlist"), query: params.get("query") } as SearchParams }),
       tap(search => console.log(search)),
+      shareReplay(1)
+    )
+
+    this.title$ = search$.pipe(map(search => {
+      switch (search.playlist) {
+        case 'institutional':
+          return "Institucional";
+        case 'live':
+          return "Emisiones pasadas";
+        default:
+          return `Resultados para '${search.query}'`;
+      }
+    }))
+
+    this.results$ = search$.pipe(
       // TODO: Por ahora mostramos solo los primeros 10 resultados
       // En un futuro evaluar la posibilidad de agregar paginación (botón de 'Mas resultados')
       switchMap(search => this.youtube.searchFor(search).pipe(take(10), toArray()))
