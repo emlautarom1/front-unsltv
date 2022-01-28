0291773b1627ac51c03433f237fc1712f78d2c30
Lautaro Emanuel <emlautarom1@gmail.com>
2022-01-27 22:00:07 -0300

    Eliminado 'hour' como 'FilterDate' válido

--- src/app/model/video-search-controls.ts
+++ src/app/model/video-search-controls.ts
@@ -1,6 +1,6 @@
 export type SortBy = "publishedAt" | "title"
 
-export type FilterDate = "any" | "hour" | "day" | "week" | "month" | "year"
+export type FilterDate = "any" | "day" | "week" | "month" | "year"
 
 export interface Controls {
     sortBy: SortBy,
