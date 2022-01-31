e99e647b006088fa7de24f04111fa9c74b186c53
Lautaro Emanuel <emlautarom1@gmail.com>
2022-01-31 18:52:26 -0300

    Fuzzy search elimina caracteres no alfanumericos

--- src/app/service/fuzzy-search.service.ts
+++ src/app/service/fuzzy-search.service.ts
@@ -6,15 +6,19 @@ import { Video } from '../model/video';
   providedIn: 'root'
 })
 export class FuzzySearchService {
+  private REGEX = new RegExp(/[\W_]+/g);
 
   constructor() { }
 
   matchesVideo(query: string, video: Video): boolean {
+    // Elimina caracteres no alfanuméricos
+    let cleanQuery = query.replace(this.REGEX, " ");
+
     const fuse = new Fuse([video], {
       keys: ["snippet.title"],
       threshold: 0.4
     })
-    let matches = fuse.search(query);
+    let matches = fuse.search(cleanQuery);
     return matches.length > 0
   }
 }
